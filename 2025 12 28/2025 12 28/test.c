808410c8efa3d671bbc43908f3ab706f6b519d25
xiao-chen-ccccc <2128442427@qq.com>
2025-12-28 16:57:47 +0800

    文件操作

---
+++ 2025 12 28/2025 12 28/test.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w+");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	int i = 0;
+//	for (i = 'a'; i <= 'z'; i++)
+//	{
+//		fputc(i, pf);
+//	}
+//	char arr[28] = { 0 };
+//	fprintf(pf,"%s","a");
+//	return 0;
+//}
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror(pf);
+//		return 1;
+//	}
+//	int i = 0;
+//	for (i = 'a'; i <= 'z'; i++)
+//	{
+//		fputc(i, pf);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	FILE*pf2=fopen("test.txt", "r");
+//	if (pf2 == NULL)
+//	{
+//		perror(pf2);
+//		return 1;
+//	}
+//	char a[33] = { 0 };
+//	for (i = 0; i <25; i++)
+//	{
+//		a[i] = getc(pf2);
+//		printf("%c", a[i]);
+//	}	
+//	
+//	fclose(pf2);
+//	pf2 = NULL;
+//	return 0;
+//}
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror(pf);
+//		return 1;
+//	}
+//	fprintf(pf, "%s", "abcdefgh");
+//	fclose(pf);
+//	pf = NULL;
+//	FILE* p2 = fopen("test.txt", "r");
+//	if(p2==NULL)
+//		{
+//		perror(p2);
+//		return 2;
+//		}
+//	int i = 0;
+//	
+//		char c[10] = { 0 };
+//	  fgets(c, 9, p2);
+//		printf("%s\n", c);
+//	fclose(p2);
+//	p2 = NULL;
+//	return 0;
+//}
