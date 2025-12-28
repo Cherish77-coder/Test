cb9da46d9707a08d0ff5d0b1d089640e746a098d
xiao-chen-ccccc <2128442427@qq.com>
2025-12-28 20:53:08 +0800

    数组名以及字符串，sizeof strlen题目标注

--- 2025 12 28/2025 12 28/test.c
+++ 2025 12 28/2025 12 28/test.c
@@ -1,99 +1,107 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<assert.h>
-char* my_strcat(char* p1,const char* p2)
-{
-	char* ret = p1;
-	while (*p1!='\0')
-		p1++;
-	assert(p1 && p2);
-	while ((*p1++ = *p2++))
-	{
-	};
-	return ret;
-}
-int main()
-{
-	char arr1[10] = "abc";
-	char arr2[4] = "def";
-	my_strcat(arr1, arr2);
-	printf("%s", arr1);
-	return 0;
-}
-
+#include<string.h>
 
 //int main()
 //{
-//	FILE* pf = fopen("test.txt", "w+");
-//	if (pf == NULL)
-//	{
-//		perror("fopen");
-//		return 1;
-//	}
-//	int i = 0;
-//	for (i = 'a'; i <= 'z'; i++)
-//	{
-//		fputc(i, pf);
-//	}
-//	char arr[28] = { 0 };
-//	fprintf(pf,"%s","a");
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a));//48
+//	printf("%d\n", sizeof(a[0][0]));//4
+//	printf("%d\n", sizeof(a[0]));//16
+//	printf("%d\n", sizeof(a[0] + 1));//表示a[1]地址，是地址所以是4或8
+//	printf("%d\n", sizeof(*(a[0] + 1)));//4
+//	printf("%d\n", sizeof(a + 1));//？ 数组名表示首元素地址是地址所以是4或8
+//	printf("%d\n", sizeof(*(a + 1)));//16
+//	printf("%d\n", sizeof(&a[0] + 1));//4
+//	printf("%d\n", sizeof(*(&a[0] + 1)));//？=a[1]
+//	printf("%d\n", sizeof(*a));//？*a=a[0],16
+//	printf("%d\n", sizeof(a[3]));//16
 //	return 0;
 //}
 //int main()
 //{
-//	FILE* pf = fopen("test.txt", "w");
-//	if (pf == NULL)
-//	{
-//		perror(pf);
-//		return 1;
-//	}
-//	int i = 0;
-//	for (i = 'a'; i <= 'z'; i++)
-//	{
-//		fputc(i, pf);
-//	}
-//	fclose(pf);
-//	pf = NULL;
-//	FILE*pf2=fopen("test.txt", "r");
-//	if (pf2 == NULL)
-//	{
-//		perror(pf2);
-//		return 1;
-//	}
-//	char a[33] = { 0 };
-//	for (i = 0; i <25; i++)
-//	{
-//		a[i] = getc(pf2);
-//		printf("%c", a[i]);
-//	}	
-//	
-//	fclose(pf2);
-//	pf2 = NULL;
+//	char* p = "abcdef";
+//	printf("%d\n", strlen(p));//6
+//	//printf("%d\n", strlen(p + 1));//地址错误
+//	//printf("%d\n", strlen(*p));//错误
+//	//printf("%d\n", strlen(p[0]));//错误
+//	//printf("%d\n", strlen(&p));//错误
+//	//printf("%d\n", strlen(&p + 1));//错误
+//	//printf("%d\n", strlen(&p[0] + 1));//错误
 //	return 0;
 //}
 //int main()
 //{
-//	FILE* pf = fopen("test.txt", "w");
-//	if (pf == NULL)
-//	{
-//		perror(pf);
-//		return 1;
-//	}
-//	fprintf(pf, "%s", "abcdefgh");
-//	fclose(pf);
-//	pf = NULL;
-//	FILE* p2 = fopen("test.txt", "r");
-//	if(p2==NULL)
-//		{
-//		perror(p2);
-//		return 2;
-//		}
-//	int i = 0;
-//	
-//		char c[10] = { 0 };
-//	  fgets(c, 9, p2);
-//		printf("%s\n", c);
-//	fclose(p2);
-//	p2 = NULL;
+//	char* p = "abcdef";
+//	printf("%d\n", sizeof(p));//p表示首元素地址是地址所以是4或8
+//	printf("%d\n", sizeof(p + 1));//下一个元素地址4或8
+//	printf("%d\n", sizeof(*p));//表示首元素1
+//	printf("%d\n", sizeof(p[0]));//1
+//	printf("%d\n", sizeof(&p));//地址4或8
+//	printf("%d\n", sizeof(&p + 1));//下一个数组地址4或8
+//	printf("%d\n", sizeof(&p[0] + 1));//表示b的地址4或8
 //	return 0;
 //}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	//printf("%d\n", strlen(arr));//6
+//	//printf("%d\n", strlen(arr + 0));//6
+//	//printf("%d\n", strlen(*arr));//错误
+//	printf("%d\n", strlen(arr[1]));//错误
+//	//printf("%d\n", strlen(&arr));//错误
+//	//printf("%d\n", strlen(&arr + 1));//错误
+//	//printf("%d\n", strlen(&arr[0] + 1));//错误
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//printf("%d\n", sizeof(arr));//表示整个元素，包含/0,7
+//printf("%d\n", sizeof(arr+0));//表示地址4或8
+//printf("%d\n", sizeof(*arr));//表示首元素1
+//printf("%d\n", sizeof(arr[1]));//表示第二个元素char，1
+//printf("%d\n", sizeof(&arr));//整个数组地址4或8
+//printf("%d\n", sizeof(&arr+1));//跳过整个数组的地址，4或8
+//printf("%d\n", sizeof(&arr[0]+1));//地址4或8
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = { 'a','b','c','d','e','f' };
+//	//printf("%d\n", strlen(arr));//没有\0所以是随机值19
+//	//printf("%d\n", strlen(arr + 0));//没有\0，随机值19
+//	//printf("%d\n", strlen(*arr));//表示首元素所以是19
+//	//printf("%d\n", strlen(arr[1]));//表示第二个元素,没有斜杠0随机值18
+//	//printf("%d\n", strlen(&arr));//表示整个素组的地址4或19
+//	//printf("%d\n", strlen(&arr + 1));//表示整个元素地址+1跳过整个数组，地址4或8，12
+//	//printf("%d\n", strlen(&arr[0] + 1));//表示第二个元素地址4或8，18
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = { 'a','b','c','d','e','f' };
+//	printf("%d\n", sizeof(arr));//表示整个数组，枚举类型不包含\0，所以是6
+//	printf("%d\n", sizeof(arr + 0));//arr+0为int类型，所以是4
+//	printf("%d\n", sizeof(*arr));//表示首元素的值1
+//	printf("%d\n", sizeof(arr[1]));//表示第二个元素为char，1
+//	printf("%d\n", sizeof(&arr));//表示整个元素的地址所以是4或8
+//	printf("%d\n", sizeof(&arr + 1));//表示地址4或8
+//	printf("%d\n", sizeof(&arr[0] + 1));//表示地址所以是4或8
+//	return 0;
+//}
+//int main()
+//{
+//	int a[] = { 1,2,3,4 };
+//	printf("%d\n", sizeof(a));//16
+//	printf("%d\n", sizeof(a + 0));//4
+//	printf("%d\n", sizeof(*a));//4或8
+//	printf("%d\n", sizeof(a + 1));//4
+//	printf("%d\n", sizeof(a[1]));//4
+//	printf("%d\n", sizeof(&a));//表示整个数组所以是16
+//	printf("%d\n", sizeof(*&a));//4
+//	printf("%d\n", sizeof(&a + 1));//4或8
+//	printf("%d\n", sizeof(&a[0]));//4
+//	printf("%d\n", sizeof(&a[0] + 1));//4
+//}
+
