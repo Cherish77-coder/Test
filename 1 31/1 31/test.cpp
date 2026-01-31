0e0f16594b1ec4f481895b9b61f3e166bf982646
xiao-chen-ccccc <2128442427@qq.com>
2026-01-31 10:42:11 +0800

    c++

---
+++ 1 31/1 31/test.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=0, b=0;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << "hello world" << endl;
+	return 0;
+}
