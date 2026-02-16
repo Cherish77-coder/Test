999c7cec60d7ee95d2ac73f20a5465496201e33e
小陈ccccc <2128442427@qq.com>
2026-02-16 10:42:08 +0000

    类和对象析构函数和构造函数，运算符重载运用的运用。
    Signed-off-by: 小陈ccccc <2128442427@qq.com>

---
+++ 2 16/Date.cpp
@@ -0,0 +1,284 @@
+﻿#include"Date.h"
+Date::Date(int year , int month , int day)
+{
+	_year = year;
+	_month = month;
+	_day = day;
+	if (!CheckDate())
+	{
+		cout << "日期非法" << endl;
+	}
+}
+void Date:: Print() const
+{
+	cout <<_year << "/" << _month << "/" << _day << endl;
+}
+bool Date::CheckDate() const
+{
+	if (_year < 0 || _month>13 || _month < 0 || _day < 0 || _day>32)
+		return false;
+	else if (GetMonthDay(_year, _month) < _day)
+		return false;
+	return true;
+}
+bool Date::operator<(const Date& d) const
+{
+	if (_year < d._year)
+		return true;
+	else if (_year == d._year)
+	{
+		if (_month < d._month)
+		{
+			return true;
+		}
+		else if (_month == d._month)
+		{
+			if (_day < d._day)
+			{
+				return true;
+			}
+		}
+	}
+	
+		return false;
+}
+bool Date::operator<=(const Date& d) const
+{
+	return *this < d || *this == d;
+}
+bool Date::operator>(const Date& d) const
+{
+	if (_year > d._year)
+		return true;
+	else if (_year == d._year)
+	{
+		if (_month > d._month)
+		{
+			return true;
+		}
+		else if (_month == d._month)
+		{
+			if (_day > d._day)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+bool Date::operator>=(const Date& d) const
+{
+	return *this >d || *this == d;
+}
+bool Date::operator==(const Date& d) const
+{
+	return _year == d._year && _month == d._month && _day == d._day;
+}
+bool Date::operator!=(const Date& d) const
+{
+	return !(_year == d._year && _month == d._month && _day == d._day);
+}
+Date& Date::operator+=(int day)
+{
+	if (day < 0)
+	{
+		*this -= (-day);
+		return *this;
+	}
+	int ret = day;
+	while (ret--)
+	{
+		int get = GetMonthDay(_year, _month);
+		if (_day < get)
+		{
+			_day++;
+		}
+		else
+		{
+			if (_month < 12)
+			{
+				_month++;
+				_day = 1;
+			}
+			else
+			{
+				_year++;
+				_month = _day = 1;
+			}
+		}
+	}
+	return *this;
+}
+Date Date::operator+(int day) const
+{
+	Date temp = *this;
+	if (day < 0)
+	{
+		temp -= (-day);
+		return temp;
+	}
+	temp += day;
+	return temp;
+}
+//Date Date::operator+(int day)
+//{
+//	Date temp = *this;
+//	int ret = day;
+//	while (ret--)
+//	{
+//		int get = GetMonthDay(temp._year, temp._month);
+//		if (temp._day < get)
+//		{
+//			temp._day++;
+//		}
+//		else
+//		{
+//			if (temp._month < 12)
+//			{
+//				temp._month++;
+//				temp._day = 1;
+//			}
+//			else
+//			{
+//				temp._year++;
+//				temp._month = temp._day = 1;
+//			}
+//		}
+//	}
+//	return temp;
+//}
+Date& Date::operator-=(int day)
+{
+	if (day < 0)
+	{
+		*this += (-day);
+		return *this;
+	}
+	_day -= day;
+	while (_day < 0)
+	{
+		if (_month > 1)
+		{
+			_month--;
+			_day += GetMonthDay(_year, _month);
+		}
+		else if (_month == 1)
+		{
+			_year--;
+			_month = 12;
+			_day += GetMonthDay(_year, _month);
+		}
+	}
+	return *this;
+}
+Date Date::operator-(int day) const
+{
+	Date temp = *this;
+	if (day < 0)
+	{
+		temp += (-day);
+		return temp;
+	}
+	temp -= day;
+	return temp;
+}
+//Date Date::operator-(int day)
+//{
+//	Date temp = *this;
+//	int ret = day;
+//	while (ret--)
+//	{
+//		//int get = GetMonthDay(_year, _month);
+//		if (temp._day > 1)
+//		{
+//			temp._day--;
+//		}
+//		else
+//		{
+//			if (temp._month > 1)
+//			{
+//				temp._month--;
+//				temp._day = GetMonthDay(temp._year, temp._month);
+//			}
+//			else if (temp._month == 1 && temp._year > 0)
+//			{
+//				temp._year--;
+//				temp._month = 12;
+//				temp._day = GetMonthDay(temp._year, temp._month);
+//			}
+//		}
+//	}
+//	return temp;;
+//}
+int Date::operator-(const Date& d) const
+{
+	Date d1(*this);
+	Date d2(d);
+	int ret = 0;
+	if (d1 > d2)
+	{
+		while (d1 != d2)
+		{
+			d1-=1;
+			ret++;
+		}
+	}
+	else if (d1 == d2)
+		return 0;
+	else {
+		while (d1 != d2)
+		{
+			d2-=1;
+			ret++;
+		}
+	}
+	return ret;
+}
+Date& Date::operator++()
+{
+	int day = GetMonthDay(_year,_month);
+	if (_day < day)
+	{
+		_day++;
+	}
+	else if (_month < 12)
+	{
+		_month++;
+		_day = 1;
+	}
+	else if (_month == 12)
+	{
+		_year++;
+		_month = _day = 1;
+	}
+	return *this;
+}
+Date Date::operator++(int)
+{
+	Date d1(*this);
+	*this += 1;
+	return d1;
+}
+Date& Date::operator--()
+{
+	*this -= 1;
+	return *this;
+}
+Date Date::operator--(int)
+{
+	Date d1(*this);
+	*this -= 1;
+	return d1;
+}
+ostream& operator<<(ostream& out, const Date& d)
+{
+	out << d._year << "年" << d._month << "月" << d._day << "日" << endl;
+	return out;
+} 
+istream& operator>>(istream& in, Date& d)
+{
+	cout << "请依次输入年月日:>";
+	in >> d._year >> d._month >> d._day;
+	return in;
+}
---
+++ 2 16/Test.cpp
@@ -0,0 +1,263 @@
+﻿#include"Date.h"
+void test1()
+{
+	Date d1(2026, 2, 30);
+}
+void test4()
+{
+	Date st(2026, 2, 30);
+	cin >> st
+		; cout << st;
+}
+void test5()
+{
+	Date d1(2026, 2, 1);
+	Date d2 = d1-5;
+	cout << d1 << d2;
+	d2 >= d1;
+}
+int main()
+{
+	//system("chcp 65001");
+	test5();
+	return 0;
+}
+//#include<iostream>
+//using namespace std;
+//#include<iostream>
+//using namespace std;
+//typedef int STDataType;
+//class Stack
+//{
+//public:
+//	Stack& operator=(const Stack& st)
+//	{
+//		if (this->_a != st._a)
+//		{
+//			STDataType* pa = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+//			if (pa == nullptr)
+//			{
+//				perror("operator::malloc fail");
+//				exit(1);
+//			}
+//			_a = pa;
+//			memcpy(_a, st._a, st._top * sizeof(STDataType));
+//			_top = st._top;
+//			_capacity = st._capacity;
+//		}
+//		return *this;
+//	}
+//	Stack(int n = 4)
+//	{
+//		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败");
+//			return;
+//		}
+//		_capacity = n;
+//		_top = 0;
+//	}
+//	Stack(const Stack& st)
+//	{
+//		_a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败!!!");
+//			return;
+//		}
+//		memcpy(_a, st._a, sizeof(STDataType) * st._top);
+//		_top = st._top;
+//		_capacity = st._capacity;
+//	}
+//	void Push(STDataType x)
+//	{
+//		if (_top == _capacity)
+//		{
+//			int newcapacity = _capacity * 2;
+//			STDataType* tmp = (STDataType*)realloc(_a, newcapacity *
+//				sizeof(STDataType));
+//			if (tmp == NULL)
+//			{
+//				perror("realloc fail");
+//				return;
+//			}
+//			_a = tmp;
+//			_capacity = newcapacity;
+//		}
+//		_a[_top++] = x;
+//	}
+//	~Stack()
+//	{
+//		cout << "~Stack()" << endl;
+//		free(_a);
+//		_a = nullptr;
+//		_top = _capacity = 0;
+//	}
+//private:
+//	STDataType* _a;
+//	size_t _capacity;
+//	size_t _top;
+//};
+//class Myqueue
+//{
+//public:
+//	void MyqueuePush()
+//	{
+//		cout << "MyqueuePush()" << endl;
+//	}
+//private:
+//	Stack Stpush;
+//	Stack Stpop;
+//};
+//int main()
+//{
+//	Stack st1(4);
+//	st1.Push(1);
+//	st1.Push(2);
+//	st1.Push(3);
+//	st1.Push(4);
+//	st1.Push(5);
+//	Stack st2;
+//	st2 = st1;
+//	return 0;
+//}
+//#include<stdio.h>
+//int add(int x, int y)
+//{
+//	return x + y;
+//}
+//void call(int x, int y, int(*call)(int ,int))
+//{
+//	printf("%d", call(x,y));
+//}
+//int main()
+//{
+//	call(1, 2, add);
+//}
+//#include<iostream>
+//using namespace std;
+//typedef int STDataType;
+//class Stack
+//{
+//public:
+//	Stack(int n = 4)
+//	{
+//		cout << "Stack(int n = 4)" << endl;
+//		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败");
+//			return;
+//		}
+//		_capacity = n;
+//		_top = 0;
+//	}
+	//Stack( const Stack& st)
+	//{
+	//	cout << "Stack(const Stack& st)" << endl;
+	//	_a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+	//	if (nullptr == _a)
+	//	{
+	//		perror("malloc申请空间失败!!!");
+	//		return;
+	//	}
+	//	memcpy(_a, st._a, sizeof(STDataType) * st._top);
+	//	_top = st._top;
+	//	_capacity = st._capacity;
+	//}
+	//void Push(STDataType x)
+	//{
+	//	if (_top == _capacity)
+	//	{
+	//		int newcapacity = _capacity * 2;
+	//		STDataType* tmp = (STDataType*)realloc(_a, newcapacity *
+	//			sizeof(STDataType));
+	//		if (tmp == NULL)
+	//		{
+	//			perror("realloc fail");
+	//			return;
+	//		}
+	//		_a = tmp;
+	//		_capacity = newcapacity;
+	//	}
+	//	_a[_top++] = x;
+	//}
+	//~Stack()
+	//{
+	//	cout << "~Stack()" << endl;
+	//	free(_a);
+	//	_a = nullptr;
+	//	_top = _capacity = 0;
+	//}
+//	~Stack()
+//	{
+//		cout << "~Stack()" << endl;
+//		free(_a);
+//		_a = nullptr;
+//		_top = _capacity = 0;
+//	}
+//private:
+//	STDataType* _a;
+//	size_t _capacity;
+//	size_t _top;
+//	};
+//class MyQueue
+//{
+//public:
+//private:
+//	Stack pushst;
+//	Stack popst;
+//};
+//int main()
+//{
+//	Stack st;
+//	st.Push(1);
+//	st.Push(1);
+//	st.Push(1);
+//	st.Push(1);
+//	st.Push(1);
+//	Stack st2(st);
+//	return 0;
+//}
+//#include"Date.h"
+//void test1()
+//{
+//	Date d1;
+//	Date d2(2015,4,5);
+//	d2 < d1;
+//	Date d3(2222, 33, 1);
+//	d1 <= d2;
+//	d1 > d2;
+//}
+//void test2()
+//{
+//	Date d1(2026, 2, 14);
+//	Date d2 = d1 - 2092;
+//	d1.Print();
+//	d2.Print();
+//}
+//void test3()
+//{
+//	Date d1(2026,12,28);
+//	d1.Print();
+//	Date& d5 = d1;
+//	d5++;
+//	d5++;
+//	d5++;
+//	d5++;
+//	d1.Print();
+//	//Date d2(2024,8,24);
+//	//Date d3 = d1++;
+//	//d1.Print();
+//	//d1--;
+//	//d1.Print();
+//	//d3.Print();
+//	//cout << d2 - d1 << endl;
+//
+//}
+//int main()
+//{
+//	test3();
+//	return 0;
+//}
